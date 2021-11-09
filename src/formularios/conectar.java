ba52f50d1662de6407a8f8f7dac6989285123f76
Brandon.FT <brandonflores.ft@gmail.com>
2021-11-09 17:11:33 -0600

    Copia de seguridad

---
+++ src/formularios/conectar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formularios;
+
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Charlie
+ */
+public class conectar {
+    Connection conect = null;
+	public Connection conexion()
+		{
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conect = DriverManager.getConnection("jdbc:mysql://localhost/consultorio","root","");
+				System.out.println("Conexion establecida al sistema");
+			} catch (ClassNotFoundException | SQLException e) {
+				System.out.println("Error de conexion");
+			}
+			return conect;
+		}
+    
+}
